91997829a58eeaa645a135e4787b6e572f3c83c6
Alex Wang <selltrends@gmail.com>
2015-12-15 18:33:23 +0800

    add grunt test framework

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+// This shows a full config file!
+module.exports = function (grunt) {
+    grunt.initConfig({
+        watch: {
+            files: 'scss/**/*.scss',
+            tasks: ['sass']
+        },
+        sass: {
+            dev: {
+                files: {
+                    'css/main.css': 'scss/main.scss'
+                }
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        'js/*.js',
+                        '*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './'
+                }
+            }
+        }
+    });
+
+    // load npm tasks
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    // define default task
+    grunt.registerTask('default', ['browserSync', 'watch']);
+};
