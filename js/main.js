fd79217a6d6b1e658b3f89748cef44c32a483f05
Alex Wang <selltrends@gmail.com>
2015-12-16 11:05:34 +0800

    add json content to html

--- js/main.js
+++ js/main.js
@@ -3,3 +3,11 @@ jQuery(document).ready(function(){
   $(this).css("background-color","yellow");
   });
 });
+
+var contentApp = angular.module('contentApp',[]);
+
+contentApp.controller('ContentListCtrl',function($scope, $http){
+  $http.get('data/content.json').success(function(data){
+    $scope.contents = data;
+  })
+})
